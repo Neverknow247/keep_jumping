ae245ffc9e4c5af24384a968d3878d5afc6798da
Nathan Bell <71198973+Neverknow247@users.noreply.github.com>
2024-02-28 04:36:54 -0500

    add dirt tileset and forest spikes

--- items/spikes.gd
+++ items/spikes.gd
@@ -1,4 +1,15 @@
 extends StaticBody2D
 
+@export var type = 0
+
+@onready var sprite_2d = $Sprite2D
+
+const sprites = [
+	preload("res://assets/art/items/forest_spikes.png"),
+	preload("res://assets/art/items/Spikes.png")
+]
+
 var damage = 1
 
+func _ready():
+	sprite_2d.texture = sprites[type]
