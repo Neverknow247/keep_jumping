2d642d7084b9885e4dd10a1bea58055d30ae567f
Nathan Bell <71198973+Neverknow247@users.noreply.github.com>
2024-06-03 15:37:59 -0400

    worked on T:3

--- ui/camera_2d.gd
+++ ui/camera_2d.gd
@@ -1,11 +1,27 @@
 extends Camera2D
 
 @onready var timer = $Timer
+@onready var small = $normal_particles/small
+@onready var medium = $normal_particles/medium
+@onready var big = $normal_particles/big
+@onready var very_big = $normal_particles/very_big
+@onready var animation_player = $AnimationPlayer
 
 var shake = 0
+var in_space = false
 
 func _ready():
 	Events.add_screenshake.connect(_add_screen_shake)
+
+func enter_space():
+	if !in_space:
+		in_space = !in_space
+		animation_player.play("particles_enter_space")
+
+func exit_space():
+	if in_space:
+		in_space = !in_space
+		animation_player.play("particles_exit_space")
 
 @warning_ignore("unused_parameter")
 func _process(delta):
